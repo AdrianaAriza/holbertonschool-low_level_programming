2cc22c0e0a07362c59ed64c189d8ed9e6c90181b
AdrianaAriza <1296@holbertonschool.com>
2019-09-19 14:22:47 -0500

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,19 @@
 int main(void)
 {
 	char a;
+	char b;
 
 	a = 'a';
 	while (a <= 'z')
 	{
 		putchar(a);
 		a++;
+	}
+	b = 'A';
+	while (b <= 'Z')
+	{
+		putchar(b);
+		b++;
 	}
 	putchar('\n');
 	return (0);
