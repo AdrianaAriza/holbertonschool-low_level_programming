87df39047a36db8b061c15929d15643d1de0e30c
AdrianaAriza <1296@holbertonschool.com>
2019-09-19 14:48:01 -0500

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Este progrma imprime el Abecedario
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	char b;
+
+	a = 'a';
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	b = 'A';
+	while (b <= 'Z')
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
