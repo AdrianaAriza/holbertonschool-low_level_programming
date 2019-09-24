d02cabc3a80532d7c3a96f27a69c47955111ce34
AdrianaAriza <1296@holbertonschool.com>
2019-09-24 11:20:46 -0500

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include"holberton.h"
+/**
+ *add - add tow numbers
+ *@a: number1
+ *@b: number2
+ *Return: int with the result of the add
+ */
+
+int add(int a, int b)
+{
+	int r = a + b;
+
+	return (r);
+}
