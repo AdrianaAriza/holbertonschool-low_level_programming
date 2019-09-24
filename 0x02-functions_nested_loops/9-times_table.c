6ca800f3ef9490005fc8ba67d1492a6cd2d69a25
AdrianaAriza <1296@holbertonschool.com>
2019-09-24 10:24:12 -0500

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include"holberton.h"
+
+/**
+ *times_table - prints up to nine table
+ *
+ *return: no return
+ */
+
+void times_table(void)
+{
+	int c1, c2, r;
+
+	c1 = 0;
+	while (c1 <= 9)
+	{
+		c2 = 0;
+		while (c2 <= 9)
+		{
+			r = c1 * c2;
+			if (r < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(r + '0');
+			}
+			else if (r >= 10)
+			{
+				_putchar(' ');
+				_putchar(r/10 +'0');
+				_putchar(r%10 + '0');
+			}
+			if (c2 != 9)
+			{
+			_putchar(',');
+			}
+			c2++;
+		}
+		_putchar('\n');
+		c1++;
+	}
+}
