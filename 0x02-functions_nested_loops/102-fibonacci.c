d10d7d6e70d0ede1d3748b66e4ba243f5edd0d8d
AdrianaAriza <1296@holbertonschool.com>
2019-09-25 16:02:26 -0500

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ *main - fibonacci
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	long int n1, n2, suma, cont;
+	
+	n1 = 0;
+	n2 = 1;
+	suma = 0;
+	cont = 0;
+
+	while(cont < 50)
+	{
+		suma = n1 + n2;
+		printf("%lld, ", suma);
+		cont++;
+		n1 = n2;
+		n2 = suma;
+	}
+	printf("\n");
+	
+}
