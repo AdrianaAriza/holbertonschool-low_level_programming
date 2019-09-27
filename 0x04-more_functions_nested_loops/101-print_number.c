bb4aab0df38e4ab52c216675be641592d06378a4
AdrianaAriza <1296@holbertonschool.com>
2019-09-26 23:27:56 -0500

    101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,26 +7,29 @@
 
 void print_number(int n)
 {
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n > 1000 && n < 1025)
-	{
-		_putchar((n / 1000) + '0');
-		_putchar(((n / 100) % 10) + '0');
-		_putchar(((n / 10) % 10) + '0');
-	}
-	else if (n >= 100 && n < 1000)
-	{
-		_putchar((n / 100) + '0');
-		_putchar(((n / 10) % 10) + '0');
-	}
-	else if (n >= 10 && n < 100)
-	{
+	if (n >= 1000000000)
+		_putchar((n / 1000000000) + '0'), n = n / 10;
+	if (n >= 100000000)
+		_putchar((n / 100000000) + '0'), n = n / 10;
+	if (n >= 10000000)
+		_putchar((n / 10000000) + '0'), n = n / 10;
+	if (n >= 1000000)
+		_putchar((n / 1000000) + '0'), n = n / 10;
+	if (n >= 100000)
+		_putchar((n / 100000) + '0'), n = n / 10;
+	if (n >= 10000)
+		_putchar((n / 10000) + '0'), n = n / 10;
+	if (n >= 1000)
+		_putchar((n / 1000) + '0'), n = n / 10;
+	if (n >= 100)
+		_putchar((n / 100) + '0'), n = n / 10;
+	if (n >= 10)
 		_putchar((n / 10) + '0');
-	}
 	_putchar((n % 10) + '0');
 }
-
