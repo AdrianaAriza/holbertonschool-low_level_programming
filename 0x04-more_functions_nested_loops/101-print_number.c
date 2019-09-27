c66774b25c357ace08858d47fb8a436a4daba422
AdrianaAriza <1296@holbertonschool.com>
2019-09-26 23:09:35 -0500

    101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,32 @@
+#include"holberton.h"
+/**
+ *print_number - prints int
+ *@n: number to be printed
+ *Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n > 1000 && n < 1025)
+	{
+		_putchar((n / 1000) + '0');
+		_putchar(((n / 100) % 10) + '0');
+		_putchar(((n / 10) % 10) + '0');
+	}
+	else if (n >= 100 && n < 1000)
+	{
+		_putchar((n / 100) + '0');
+		_putchar(((n / 10) % 10) + '0');
+	}
+	else if (n >= 10 && n < 100)
+	{
+		_putchar((n / 10) + '0');
+	}
+	_putchar((n % 10) + '0');
+}
+
