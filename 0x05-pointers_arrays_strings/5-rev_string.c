f2b603167c65228417d71fb0e1259a397aeb04fa
AdrianaAriza <1296@holbertonschool.com>
2019-10-01 10:39:17 -0500

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+/**
+ *rev_string - reverse a string
+ *@s: string to reverse
+ *Return: void
+ */
+
+#include<stdio.h>
+
+void rev_string(char *s)
+{
+	char a[100];
+
+	int i = 0, m = 0;
+
+	while (s[i] != 0)
+	{
+		a[i] = s[i];
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		*(s + m) = a[i];
+		i--;
+		m++;
+	}
+}
+
+
+
+
