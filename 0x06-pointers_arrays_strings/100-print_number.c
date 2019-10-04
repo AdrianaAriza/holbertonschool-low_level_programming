c216f7f61e7fc2393595baaf3ffc4bc7bb8b4d28
AdrianaAriza <1296@holbertonschool.com>
2019-10-03 23:44:19 -0500

    100

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,34 @@
+#include"holberton.h"
+/**
+ *print_number - print number
+ *@n: number to pritn;
+ *Return: void
+ */
+void print_number(int n)
+{
+	int b = 1, i = 0, j;
+
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+	while (n / b > 9 || n / b < -9)
+	{
+		b = b * 10;
+		i++;
+	}
+	for (j = 0; j <= i; j++)
+	{
+		if (n < 0)
+		{
+			_putchar((-n / b) + '0');
+		}
+		else
+		{
+			_putchar((n / b) + '0');
+		}
+		n = n % b;
+		b = b / 10;
+	}
+
+}
