5863e691d54bccf9a3c5d2a5066ffb030d89ad12
AdrianaAriza <1296@holbertonschool.com>
2019-10-03 19:47:13 -0500

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+/**
+ *cap_string - capitalize the first letter of each word
+ *@s: the string
+ *Return: s
+ */
+char *cap_string(char *s)
+{
+	int i = 0, n = 0;
+	char a[13] = {32, '\t', '\n', 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			for (n = 0; n < 13; n++)
+			{
+				if (a[n] == s[i - 1])
+				{
+					s[i] = s[i] - 32;
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
+}
