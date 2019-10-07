b40ec0cbe520d89e674da353a230d18f32cc2042
AdrianaAriza <1296@holbertonschool.com>
2019-10-07 13:55:32 -0500

    4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+/**
+ *_strpbrk - search a string
+ *@s: string
+ *@accept: the string to find
+ *Return: p
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *p;
+	int h = 0, i = 0, bandera = 0;
+
+	while (s[h] != '\0' && bandera != 1)
+	{
+		i = 0;
+		while (accept[i] != '\0' && accept[i] != s[h])
+		{
+			i++;
+		}
+		if (s[h] == accept[i])
+		{
+			p = (s + h);
+			bandera = 1;
+		}
+		h++;
+	}
+	return (p);
+}
