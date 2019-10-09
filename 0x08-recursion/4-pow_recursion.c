fae80c67d441751dd374189c09c22e256273ce17
AdrianaAriza <1296@holbertonschool.com>
2019-10-09 18:28:09 -0500

    4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+/**
+ *_pow_recursion - pow a number
+ *@x: teh number
+ *@y: the pow
+ *Return: int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, --y));
+}
