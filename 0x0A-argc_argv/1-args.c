71b1938b873d32ab13d11adf4fd9433dc82ca7f9
AdrianaAriza <1296@holbertonschool.com>
2019-10-12 22:17:31 -0500

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *main - the number of parameters
+ *@argc: number of parameters
+ *@argv: parameters
+ *Return: 0;
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
