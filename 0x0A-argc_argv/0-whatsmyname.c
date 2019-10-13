23fae08335097060b3914bd9f2236eb0fcb0cc45
AdrianaAriza <1296@holbertonschool.com>
2019-10-12 22:15:16 -0500

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *main - the name of the file
+ *@argc: number of parameters
+ *@argv: parameters
+ *Return: 0;
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
