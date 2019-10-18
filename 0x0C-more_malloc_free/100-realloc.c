fcb315be3e35c757e2c0ea06c70ea7b439f68e00
AdrianaAriza <1296@holbertonschool.com>
2019-10-17 22:03:57 -0500

    100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -6,25 +6,22 @@
  *@new_size: new_size
  *Return: new puntero
  */
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *newp;
 	unsigned int i = 0;
 	char *ptr1;
 
+	if (new_size == old_size)
+		return (ptr);
 	if (ptr == NULL)
 	{
 		newp = malloc(new_size);
-		free(ptr);
 		return ((void *)(newp));
 	}
 	ptr1 = (char *)ptr;
-	if (new_size == old_size)
-		return (ptr);
 	if (new_size == 0 && ptr != NULL)
 	{
-		free(ptr);
 		return (NULL);
 	}
 	newp = malloc(new_size);
@@ -32,7 +29,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	if (ptr != NULL)
 	{
-		for (i = 0; i <= old_size; i++)
+		for (i = 0; i < old_size && i < new_size; i++)
 			newp[i] = ptr1[i];
 	}
 	free(ptr);
