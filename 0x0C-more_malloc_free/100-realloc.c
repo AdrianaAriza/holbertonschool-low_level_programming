fb8c28abf925ca76520010b184bcc5d2df21ec69
AdrianaAriza <1296@holbertonschool.com>
2019-10-17 21:26:11 -0500

    100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -31,7 +31,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	if (ptr != NULL)
 	{
-		for (i = 0; i <= old_size && i <= new_size; i++)
+		for (i = 0; i <= old_size; i++)
 			newp[i] = ptr1[i];
 	}
 	free(ptr);
