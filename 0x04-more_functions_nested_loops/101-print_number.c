d159d56916e6a9c55d5a1c86dc7b628ff8e4b7a9
AdrianaAriza <1296@holbertonschool.com>
2019-10-17 23:20:54 -0500

    101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,39 +1,34 @@
 #include"holberton.h"
 /**
- *print_number - prints int
- *@n: number to be printed
+ *print_number - print number
+ *@n: number to pritn;
  *Return: void
  */
 void print_number(int n)
 {
-	int div = 1000000000, l = 10, i = 1, neg;
+	int b = 1, i = 0, j;
 
-	if (n == 0)
-	{
-		_putchar('0');
-	}
 	if (n < 0)
 	{
 		_putchar('-');
 	}
-	while (n != 0 && (n / div) == 0)
+	while (n / b > 9 || n / b < -9)
 	{
-		l = (l - 1);
-		div = div / 10;
+		b = b * 10;
+		i++;
 	}
-	for (l = l - 1; l > 0; l--)
+	for (j = 0; j <= i; j++)
 	{
-		i = i * 10;
-	}
-	while (i > 0 && n != 0)
-	{
-		neg = n / i;
-		if (neg < 0)
+		if (n < 0)
 		{
-			neg = -neg;
+			_putchar(-(n / b) + '0');
 		}
-		_putchar(neg + '0');
-		n = n % i;
-		i = (i / 10);
+		else
+		{
+			_putchar((n / b) + '0');
+		}
+		n = n % b;
+		b = b / 10;
 	}
+
 }
