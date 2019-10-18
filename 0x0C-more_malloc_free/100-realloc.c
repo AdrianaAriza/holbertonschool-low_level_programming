8de27f6b316063bcdb6b530f3ec4f8735b177a75
AdrianaAriza <1296@holbertonschool.com>
2019-10-17 22:25:04 -0500

    no molestar

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -6,26 +6,25 @@
  *@new_size: new_size
  *Return: new puntero
  */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *newp;
 	unsigned int i = 0;
 	char *ptr1;
 
+	if (new_size == old_size)
+		return (ptr);
 	if (ptr == NULL)
 	{
 		newp = malloc(new_size);
 		return ((void *)(newp));
 	}
+	ptr1 = (char *)ptr;
 	if (new_size == 0 && ptr != NULL)
 	{
-		return (NULL);
-	}
-	ptr1 = (char *)ptr;
-	if (new_size == old_size)
-	{
 		free(ptr);
-		return (ptr);
+		return (NULL);
 	}
 	newp = malloc(new_size);
 	if (newp == NULL)
