860f5fe2573bcf8ff6f59da889dc1e979af230c5
AdrianaAriza <1296@holbertonschool.com>
2019-10-21 20:26:06 -0500

    2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include"dog.h"
+/**
+ *print_dog - prints dogs info
+ *@d: struct
+ *Return: void;
+ */
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\n", d->name);
+	printf("Age: %lf\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
