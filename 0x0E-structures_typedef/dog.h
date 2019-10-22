2bda395565d7415dda273511eb83be02c58fbed9
AdrianaAriza <1296@holbertonschool.com>
2019-10-21 19:08:33 -0500

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Dog
+ * @name: Dog's name
+ * @age: Age
+ * @owner: Name of the owner
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif /* DOG_H */
