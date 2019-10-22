cfe80b8efb761cf6806f360646eca34ab899bb0e
AdrianaAriza <1296@holbertonschool.com>
2019-10-21 22:18:24 -0500

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,15 +5,16 @@
  *@name: name
  *@age: age
  *@owner: owner
- Return: dog created dog1;
+ * Return: dog1;
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog1;
+
 	dog1 = malloc(sizeof(dog_t));
-	if(!dog1)
-		return NULL;
+	if (!dog1)
+		return (NULL);
 	dog1->name  = name;
 	dog1->age  = age;
 	dog1->owner  = owner;
