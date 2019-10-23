f50caf8b92577079973028ba3a381813b992d608
AdrianaAriza <1296@holbertonschool.com>
2019-10-23 15:13:46 -0500

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ *int_index - passing eachs element of an array
+ *@array: array
+ *@size: size
+ *@cmp: function
+ *Return: void
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+	if (array != NULL && cmp != NULL)
+	{
+		for (; i < size; i++)
+		{
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
+		}
+		return (-1);
+	}
+	return (0);
+}
