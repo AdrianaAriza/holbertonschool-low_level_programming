df6075f035800b49aa609b25faf0fc78a3da8ee3
AdrianaAriza <1296@holbertonschool.com>
2019-10-23 14:08:41 -0500

    Betty

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,12 @@
+/**
+ *print_name - print a name
+ *@name: name
+ *@f: print function
+ *Return: void
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	if(!name)
+	if (!name)
 		return;
 	f(name);
 }
