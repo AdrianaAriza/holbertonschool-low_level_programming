9d0598f31b38402a2da704c4690de132c8aeb190
AdrianaAriza <1296@holbertonschool.com>
2019-10-27 19:41:51 -0500

    separadores

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,28 +9,37 @@ void print_all(const char * const format, ...)
 {
 	int i = 0;
 	va_list valist;
+	char *str, *aux = "";
 
 	va_start(valist, format);
 	while (format[i])
 	{
+		aux = "";
+		if (i >= 0 && format[i + 1] != '\0')
+			aux = ", ";
 		switch (format[i])
 		{
 		case 'c':
-			printf("%c", (char) va_arg(valist, int));
+			printf("%c%s", (char) va_arg(valist, int), aux);
 			break;
 		case 'i':
-			printf("%d", va_arg(valist, int));
+			printf("%d%s", va_arg(valist, int), aux);
 			break;
 		case 'f':
-			printf("%f", (float) va_arg(valist, double));
+			printf("%f%s", (float) va_arg(valist, double), aux);
 			break;
 		case 's':
-			printf("%s", va_arg(valist, char *));
+			str =  va_arg(valist, char *);
+			if (!str)
+				printf("%p%s", str, aux);
+			else
+				printf("%s%s", str, aux);
+			break;
+		default:
 			break;
 		}
-		if (format[i + 1] && i > 0)
-			printf(", ");
 		i++;
 	}
+	va_end(valist);
 	printf("\n");
 }
