9867f4e656e3aa5efafd4345a4d796da5febb45f
Adriana Ariza <1296@holbertonschool.com>
2019-10-31 23:25:11 -0500

    [Fix]: When str = NULL

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,7 +1,7 @@
 #include"lists.h"
 #include<string.h>
 /**
- *add_node - add new element at the end
+ *add_node_end - add new element at the end
  *@head: head of the list
  *@str: str element
  *Return: pointer to new element
@@ -12,16 +12,26 @@ list_t *add_node_end(list_t **head, const char *str)
 	int i = 0;
 
 	aux = *head;
-	while (str[i] != '\0')
-		i++;
+
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-	new->str = strdup(str);
-	new->len = i;
-	new->next = NULL;
-	if(*head == NULL)
+	if (str != NULL)
+	{
+		while (str[i] != '\0')
+			i++;
+		new->str = strdup(str);
+		new->len = i;
+		new->next = NULL;
+	}
+	else
+	{
+		new->str = NULL;
+		new->len = 0;
+		new->next = NULL;
+	}
+	if (*head == NULL)
 	{
 		*head = new;
 		return (new);
