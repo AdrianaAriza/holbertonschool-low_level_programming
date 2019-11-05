2274abe887333f3b8643b7dbfe9f1971861ea2c4
AdrianaAriza <1296@holbertonschool.com>
2019-11-05 14:06:40 -0500

    [Feature]: Reverse

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,33 @@
+#include"lists.h"
+/**
+ *reverse_listint - reverse a list
+ *@head: header
+ *Return: header of list in reverse
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	int cont = 0, i = 0;
+	listint_t *a_head, *aux;
+
+	a_head = *head;
+	while (a_head->next != NULL)
+	{
+		a_head = a_head->next;
+		cont++;
+	}
+	while (cont > 0)
+	{
+		i = 0;
+		aux = *head;
+		while (i < (cont - 1))
+		{
+			aux = aux->next;
+			i++;
+		}
+		aux->next->next = aux;
+		cont--;
+	}
+	aux->next->next = NULL;
+	*head = a_head;
+	return (*head);
+}
