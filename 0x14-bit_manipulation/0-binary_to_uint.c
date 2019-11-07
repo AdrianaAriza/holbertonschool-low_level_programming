a100865f102799d8adadbd587632bfb4d51fffa8
AdrianaAriza <1296@holbertonschool.com>
2019-11-07 14:50:29 -0500

    [Fix]: Betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,29 @@
 #include"holberton.h"
-
+/**
+ *_pow - pow in 2 base
+ *@i: power
+ *Return: int
+ */
 int _pow(int i)
 {
 	int pow = 1, j = 0;
 
 	for (; j < i; j++)
-	{
 		pow = pow * 2;
-	}
 	return (pow);
 }
-
+/**
+ *binary_to_uint - converts
+ *@b: powerbinary number
+ *Return: unsigned int
+ */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int r = 0;
 	int i = 0, base = 0;
 	char *bc = (void *)b;
-	
-	if(!b)
+
+	if (!b)
 		return (r);
 	for (; bc[i]; i++)
 		;
