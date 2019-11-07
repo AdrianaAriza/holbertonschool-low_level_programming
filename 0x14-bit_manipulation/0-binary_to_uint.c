70257aeab1806d6a553c255d9eb6406d16367ec3
AdrianaAriza <1296@holbertonschool.com>
2019-11-07 14:40:51 -0500

    [Feature]: Binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include"holberton.h"
+
+int _pow(int i)
+{
+	int pow = 1, j = 0;
+
+	for (; j < i; j++)
+	{
+		pow = pow * 2;
+	}
+	return (pow);
+}
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int r = 0;
+	int i = 0, base = 0;
+	char *bc = (void *)b;
+	
+	if(!b)
+		return (r);
+	for (; bc[i]; i++)
+		;
+	i--;
+	for (; bc[i]; i--, base++)
+	{
+		if (bc[i] != 48 && bc[i] != 49)
+			return (0);
+		if (bc[i] == 49)
+			r = r + _pow(base);
+	}
+	return (r);
+}
