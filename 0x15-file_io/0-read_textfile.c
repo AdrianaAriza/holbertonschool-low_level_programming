d41289f875b49f499ab2fa702e3ba64bc0fc0724
AdrianaAriza <1296@holbertonschool.com>
2019-11-12 13:50:29 -0500

    [Fix]: When write fails - Betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,10 +13,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename)
 		return (0);
-	o = open (filename, letters);
-	if(!o)
+	o = open(filename, letters);
+	if (!o)
 		return (0);
-	while(i < letters && r != 0)
+	while (i < letters && r != 0)
 	{
 		r = read(o, &buffer, 1);
 		if (r == -1)
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		{
 			w = write(1, &buffer, 1);
 			if (w == -1)
-				return(0);
+				return (0);
 			i++;
 		}
 	}
