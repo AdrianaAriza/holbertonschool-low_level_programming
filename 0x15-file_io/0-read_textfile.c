7bf675dc595031df4eb6528326edb0e170d7bda1
AdrianaAriza <1296@holbertonschool.com>
2019-11-12 22:09:20 -0500

    [Fix]: Buffer

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,22 +9,22 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	size_t i = 0;
 	int o = 0, w = 0, r = 0;
-	char *buffer;
+	char buffer[1];
 
 	if (!filename)
 		return (0);
 	o = open(filename, 0);
 	if (o == -1)
 		return (0);
-	r = read(o, &buffer, 1);
+	r = read(o, buffer, 1);
 	if (r == -1)
 		return (0);
 	while (i < letters && r != 0)
 	{
-		w = write(STDOUT_FILENO, &buffer, 1);
-		if (w == -1)
+		w = write(STDOUT_FILENO, buffer, 1);
+		if (w == -1 || w != r)
 			return (0);
-		r = read(o, &buffer, 1);
+		r = read(o, buffer, 1);
 		if (r == -1)
 			return (0);
 		i++;
@@ -32,7 +32,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (r == 0)
 		i++;
 	r = close(o);
-	if (r == -1)
-		return (0);
+
 	return (i);
 }
