82c81912d89f81b20f2ea1e748e291a03aecec71
Adriana Ariza <12986@holbertonschool.com>
2019-12-15 14:40:23 -0500

    [Upload]: Task4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include"lists.h"
+/**
+ *free_dlistint - free lsit
+ *@head: head
+ *Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	if (head)
+	{
+		while (head->next)
+		{
+			head = head->next;
+			free(head->prev);
+		}
+		free(head);
+	}
+}
