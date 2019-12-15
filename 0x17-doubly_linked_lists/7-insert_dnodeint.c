c7e5393de44f40e421728a7542a311df686e9583
Adriana Ariza <12986@holbertonschool.com>
2019-12-15 15:29:36 -0500

    [Upload]: Task7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,40 @@
+#include"lists.h"
+/**
+ *insert_dnodeint_at_index - insert node at idx
+ *@idx: index
+ *@n: n element of the structure
+ *@h: header
+ *Return: address of new element
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *aux, *new;
+	unsigned int i = 0;
+
+	if (!h)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	aux = *h;
+	if (!idx)
+	{
+		new->n = n;
+		new->next = aux;
+		*h = new;
+	}
+	else
+	{
+		while (i < (idx - 1))
+		{
+			aux = aux->next;
+			i++;
+			if (!aux)
+				return (NULL);
+		}
+		new->n = n;
+		new->next = aux->next;
+		aux->next = new;
+	}
+	return (new);
+}
