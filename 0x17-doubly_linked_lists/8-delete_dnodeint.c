239f26221e39c42dbf34dd73d33b96ee73a8adce
AdrianaAriza <55102581+AdrianaAriza@users.noreply.github.com>
2019-12-15 20:04:22 -0500

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,10 +10,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *aux, *aux2;
 	unsigned int i = 0;
 
-	aux = *head;
 	if (!*head || !head)
 		return (-1);
-
 	if (!index)
 	{
 		if (aux->next)
