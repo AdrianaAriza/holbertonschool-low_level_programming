a4f9304f6b35a3c453b25aaa6a90fd2dceef904d
Adriana Ariza <1296@holbertonschool.com>
2020-01-18 17:06:06 -0500

    [Fix]: If key exists

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,20 +8,36 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node;
+	hash_node_t *new_node, *aux;
 	unsigned long int idx = 0;
 
-	if (!ht || !key)
-		return (0);
-	new_node = malloc(sizeof(hash_node_t));
-	if (!new_node)
+	if (!ht || !key || !value || strlen(key) == 0)
 		return (0);
 	idx  = key_index((unsigned char *) key, ht->size);
 	if (!ht->array[idx])
+	{
+		new_node = malloc(sizeof(hash_node_t));
+		if (!new_node)
+			return (0);
 		new_node->next = NULL;
+		ht->array[idx] = new_node;
+	}
 	else
-		new_node->next = ht->array[idx];
-	ht->array[idx] = new_node;
+	{
+		aux = ht->array[idx];
+		while (aux && strcmp(aux->key, key) != 0)
+			aux = aux->next;
+		if (aux)
+			;
+		else
+		{
+			new_node = malloc(sizeof(hash_node_t));
+			if (!new_node)
+				return (0);
+			new_node->next = ht->array[idx];
+			ht->array[idx] = new_node;
+		}
+	}
 	new_node->key = (char *)key;
 	new_node->value = (char *)value;
 	return (1);
