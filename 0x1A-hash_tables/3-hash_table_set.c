4f448e2fb0db29b335de1f923344f20623adabbb
Adriana Ariza <1296@holbertonschool.com>
2020-01-18 17:38:06 -0500

    [Fix]: reorganized

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,30 +14,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht || !key || !value || strlen(key) == 0)
 		return (0);
 	idx  = key_index((unsigned char *) key, ht->size);
-	if (!ht->array[idx])
+	aux = ht->array[idx];
+	while (aux && strcmp(aux->key, key) != 0)
+		aux = aux->next;
+	if (aux)
 	{
-		new_node = malloc(sizeof(hash_node_t));
-		if (!new_node)
-			return (0);
-		new_node->next = NULL;
-		ht->array[idx] = new_node;
+		aux->value = strdup(value);
+		return (1);
 	}
-	else
-	{
-		aux = ht->array[idx];
-		while (aux && strcmp(aux->key, key) != 0)
-			aux = aux->next;
-		if (aux)
-			;
-		else
-		{
-			new_node = malloc(sizeof(hash_node_t));
-			if (!new_node)
-				return (0);
-			new_node->next = ht->array[idx];
-			ht->array[idx] = new_node;
-		}
-	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+	new_node->next = ht->array[idx];
+	ht->array[idx] = new_node;
 	new_node->key = (char *)key;
 	new_node->value = strdup(value);
 	return (1);
