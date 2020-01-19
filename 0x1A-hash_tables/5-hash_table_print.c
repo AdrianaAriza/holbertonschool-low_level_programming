b3a14cb062427445f64132a79357b99e676084f2
Adriana Ariza <1296@holbertonschool.com>
2020-01-18 19:48:06 -0500

    [Upload]: Task5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include"hash_tables.h"
+/**
+ *hash_table_print - prints hash table
+ *@ht: hash table
+ *Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t **aux, *aux2;
+	unsigned int i = 0, cont  = 0, flag = 0;
+
+	if (!ht)
+		return;
+	aux = ht->array;
+
+	putchar(123);
+	while (cont < ht->size)
+	{
+		aux2 = aux[i];
+		while (aux2)
+		{
+			if (flag)
+				printf(", ");
+			printf("'%s': '%s'", aux2->key, aux2->value);
+			flag = 1;
+			if (aux2->next)
+				printf(", ");
+			aux2 = aux2->next;
+		}
+		aux++;
+		cont++;
+	}
+	putchar(125);
+	putchar('\n');
+}
